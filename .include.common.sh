8f6f41aea2bc8697b44ce575655c9f268b6e2c12
hastmu <gh-hastmu@gmx.de>
2024-11-08 17:16:03 +0100

    update web

--- .include.common.sh
+++ .include.common.sh
@@ -1,4 +1,7 @@
 declare -A DIST
 DIST["stable.all"]="repo/dist/stable/main/noarch"
 DIST["unstable.all"]="repo/dist/unstable/main/noarch"
+DIST["stable.pool.all"]="repo/pool/stable-noarch"
+DIST["unstable.pool.all"]="repo/pool/unstable-noarch"
 DIST["root"]="$(pwd)"
+
