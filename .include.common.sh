6b984d9b3046aa2cfaacfd26aef75d7852a1e33d
hastmu <gh-hastmu@gmx.de>
2024-11-10 08:57:15 +0100

    update web

--- .include.common.sh
+++ .include.common.sh
@@ -1,10 +1,10 @@
 declare -A DIST
-DIST["stable.all"]="repo/dist/stable/main/noarch"
-DIST["unstable.all"]="repo/dist/unstable/main/noarch"
+DIST["stable.all"]="repo/dist/stable/main/all"
+DIST["unstable.all"]="repo/dist/unstable/main/all"
 DIST["stable.release"]="repo/dist/stable"
 DIST["unstable.release"]="repo/dist/unstable"
-DIST["stable.pool.all"]="repo/pool/stable-noarch"
-DIST["unstable.pool.all"]="repo/pool/unstable-noarch"
+DIST["stable.pool.all"]="repo/pool/stable-all"
+DIST["unstable.pool.all"]="repo/pool/unstable-all"
 DIST["root"]="$(pwd)"
 
 declare -A ARCH
---
+++ setup.hastmu.repo.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ $(id -u) -ne 0 ]
+then
+   echo "Sorry i have to run as root"
+   exit 1
+fi
+
+if [ -z "$1" ]
+then
+   echo "which branch? (stable|unstable)"
+   read BRANCH
+else
+   BRANCH="$1"
+fi
+
+if [ "${BRANCH}" == "stable" ] || [ "${BRANCH}" == "unstable" ]
+then
+   base="https://hastmu.github.io"
+   if wget -O /etc/apt/trusted.gpg.d/hastmu.gpg ${base}/hastmu.gpg
+   then
+      echo "deb ${base}/repo/dist ${BRANCH} main" > /etc/apt/sources.list.d/hastmu.list
+      echo "hastmu repo enabled"
+      exit 0
+   else
+      echo "- gpg key download failed."
+      exit 1
+   fi
+else
+   echo "only 'stable' and 'unstable' are valid branches"
+   exit 1
+fi
+
+
+
