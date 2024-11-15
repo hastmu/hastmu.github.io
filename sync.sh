f078496ac6a564039ddb5217df7c2d034d056542
hastmu <gh-hastmu@gmx.de>
2024-11-15 16:48:24 +0100

    update web

--- sync.sh
+++ sync.sh
@@ -55,8 +55,8 @@ BLOCK
          for arch in all
          do
             echo "- scan packages of ${arch} for ${tarch} in ${DIST[${BRANCH}.pool.${arch}]#*/}"
-            [ ! -x "${DIST[${BRANCH}.${tarch}]#*/}" ] && mkdir -p "${DIST[${BRANCH}.${tarch}]#*/}"
             echo "" > "${DIST[${BRANCH}.${tarch}]#*/}/Packages"
+            continue
             ( cd "${DIST[${BRANCH}.pool.${arch}]%%/*}" ; pwd;  dpkg-scanpackages --arch "${arch}" "${DIST[${BRANCH}.pool.${tarch}]#*/}" >> "${DIST[${BRANCH}.${tarch}]#*/}/Packages" )
             echo "- build contents..."
             ( cd "${DIST[${BRANCH}.pool.${arch}]%%/*}" ; apt-ftparchive contents "${DIST[${BRANCH}.pool.${arch}]#*/}" > "${DIST[${BRANCH}.${tarch}]#*/}/Contents-${tarch}" )
